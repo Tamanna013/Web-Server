e1b355c93f647ce6d987b799f0c255c1b7e7678d
TashxMG <149794391+Tamanna013@users.noreply.github.com>
2025-01-15 12:05:02 +0530

    Add files via upload

---
+++ Server.c
@@ -0,0 +1,2 @@
+#include"Server.h"
+struct Server server_constructor(int domain, int service, int protocol, unsigned long interface, int port, int backlog)
---
+++ Server.h
@@ -0,0 +1,19 @@
+#ifndef Server_h
+#define Server_h
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+struct Server{
+	int domain;
+	int service;
+	int protocol;
+	unsigned long interface;
+	int port;
+	int backlog;
+	struct sockaddr_in address;
+	void (*launch)(void);
+};
+struct Server server_constructor(int domain, int service, int protocol, unsigned lon interface, int port, int backlog, void(*launch)(void));
+
+
+#endif
